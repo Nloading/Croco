b9df15d1def30051be9738225f019e9054371cb3
Sala <saladzenika727@gmail.com>
2025-11-02 04:46:51 +0400

    imported wheel component to offerscomponent

--- croco-app/src/app/pages/offers/offers.component.ts
+++ croco-app/src/app/pages/offers/offers.component.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { WheelComponent } from "./wheel/wheel.component";
 
 export type WeekType = 'I' | 'II' | 'III' | 'IV' | 'ALL';
 
@@ -15,51 +16,11 @@ export interface LeaderboardItem {
 @Component({
   selector: 'app-offers',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink],
+  imports: [CommonModule, FormsModule, RouterLink, WheelComponent],
   templateUrl: './offers.component.html',
   styleUrls: ['./offers.component.css'],
 })
 export class OffersComponent {
-  // ------- WHEEL -------
-  wheelNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  inputNumber = signal<number | null>(null);
-  rotationAngle = signal(0);
-  errorMessage = signal('');
-  private currentRotation = 0; // track total rotation
-
-spinWheel() {
-  const value = this.inputNumber();
-  const sectors = this.wheelNumbers.length;
-
-  if (!value || value < 1 || value > sectors) {
-    this.errorMessage.set(`Enter number between 1 and ${sectors}`);
-    return;
-  }
-
-  this.errorMessage.set('');
-
-  const sectorAngle = 360 / sectors;
-  const index = value - 1;
-
-  const extraSpins = 360; // 1 full spin clockwise
-
-  // Reset wheel virtually to 1, then spin to chosen number
-  const targetRotation = - (extraSpins + index * sectorAngle);
-
-  // Apply rotation
-  this.rotationAngle.set(targetRotation);
-}
-
-
-
-
-
-
-
-
-
-
-
   // ------- LEADERBOARD -------
   leaderboard = signal<LeaderboardItem[]>(this.generateLeaderboard());
   filterWeek = signal<WeekType>('ALL');
@@ -72,8 +33,8 @@ spinWheel() {
     weeks.forEach((w) => {
       for (let i = 1; i <= 10; i++) {
         list.push({
-          customerId: Math.floor(Math.random() * 10000),
-          loginName: `User${Math.floor(Math.random() * 1000)}`,
+          customerId: Math.floor(Math.random() * 100),
+          loginName: `User${Math.floor(Math.random() * 100)}`,
           place: i,
           week: w,
         });
--- croco-app/src/app/pages/offers/wheel/wheel.component.ts
+++ croco-app/src/app/pages/offers/wheel/wheel.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input, NgModule, signal } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-wheel',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './wheel.component.html',
   styleUrl: './wheel.component.css'
 })
 export class WheelComponent {
+  @Input() numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+  inputNumber = signal<number | null>(null);
+  rotationAngle = signal(0);
+  errorMessage = signal('');
+
+  spinWheel() {
+    const value = this.inputNumber();
+    const sectors = this.numbers.length;
+
+    if (!value || value < 1 || value > sectors) {
+      this.errorMessage.set(`Enter number between 1 and ${sectors}`);
+      return;
+    }
+
+    this.errorMessage.set('');
+
+    const sectorAngle = 360 / sectors;
+    const index = value - 1;
+    const extraSpins = 360; // 1 full spin
+
+    const targetRotation = -(extraSpins + index * sectorAngle);
+
+    this.rotationAngle.set(targetRotation);
+  }
 }
